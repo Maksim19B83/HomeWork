00a74437c81fbd65c79d44b1c9124e6348c51080
Максим Боев <maksimboev1983@mail.ru>
2023-06-18 11:46:13 +0300

    Решили задачи 37 и 35

--- LESSON/HW_3/HW_5/Program.cs
+++ LESSON/HW_3/HW_5/Program.cs
@@ -128,3 +128,84 @@
 // PrintArray(Array);
 // FindNumberInArray(Array, num);
 
+// Задача 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-100,101);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     foreach (var item in array)
+//     { 
+//         System.Console.Write($"{item} ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// int FindAndMemo(int[] array)
+// {
+//     int count = 0;
+//     for(int i=0; i < array.Length; i++)
+//     {
+//         if (array[i] > 9 && array[i] < 100)
+//            count ++; 
+//     }
+//     return count;  
+// }
+
+// Console.Clear();
+
+// int[] array = new int [123];
+
+// FillArray(array);
+// PrintArray(array);
+// System.Console.WriteLine(FindAndMemo(array));
+
+// Задача 37. Найдите произведение пар чисел в одномерном массиве.
+// парой считаем первый и последний элемент, второй и предпоследний и тд. результат записать в новом массиве.
+
+void FillArray(int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        array[i] = new Random().Next(-100, 101); 
+    } 
+} 
+ 
+void PrintArray(int[] array) 
+{ 
+    foreach (var item in array) 
+    { 
+        System.Console.Write($"{item} "); 
+    } 
+    System.Console.WriteLine(); 
+} 
+ 
+void ProizArray (int[] array, int[] arrayProiz) 
+{ 
+    int size = array.Length/2; 
+    for (int i = 0; i < size; i++) 
+    { 
+        arrayProiz[i] = array[i] * array[array.Length-1-i]; 
+    } 
+    if (size%2==1) 
+        arrayProiz[arrayProiz.Length-1] = array[array.Length/2]; 
+    System.Console.WriteLine(); 
+} 
+ 
+Console.Clear(); 
+System.Console.Write("Введите диапазон массива: "); 
+int size = Convert.ToInt32(System.Console.ReadLine()); 
+ 
+int[] array = new int[size]; 
+int[] arrayProiz = new int[size/2 + size%2]; 
+ 
+FillArray(array); 
+PrintArray(array); 
+ProizArray(array, arrayProiz); 
+PrintArray(arrayProiz);
